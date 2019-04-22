7eb7955a3915e4263d156d765abe9880c62306f3
sivanpayyadakath <sivan.payyadakath@gmail.com>
2019-04-22 13:37:11 +0900

    UINav Controller added

--- SoundBae/ViewController.swift
+++ SoundBae/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("hello view will appear")
+    }
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button")
+        recordingLabel.text = "Recording....."
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stopped")
+        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        
+    }
 }
 
