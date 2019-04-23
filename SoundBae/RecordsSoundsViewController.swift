fa4e559fc9873bef5dca5acacf55c3062e7ca47b
sivanpayyadakath <sivan.payyadakath@gmail.com>
2019-04-23 14:30:23 +0900

    f -swift

--- SoundBae/RecordsSoundsViewController.swift
+++ SoundBae/RecordsSoundsViewController.swift
@@ -72,9 +72,10 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate  {
         }
     }
  
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "stopRecording" {
-            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let playSoundsVC: PlaySoundsViewController = segue.destination as! PlaySoundsViewController
             let recordedAudioURL = sender as! URL
             playSoundsVC.recordedAudioURL = recordedAudioURL
         }
