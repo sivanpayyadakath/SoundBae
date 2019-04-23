a84e3004f221c83f2a53bd0908c673cc77161abd
sivanpayyadakath <sivan.payyadakath@gmail.com>
2019-04-23 09:36:59 +0900

    adding record-sounds

---
+++ SoundBae/PlaySoundsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlaySoundsViewController.swift
+//  SoundBae
+//
+//  Created by Sivan.Payyadakath on 2019/04/22.
+//  Copyright © 2019 Sivan.Payyadakath. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
